29f01b7b26ad4aa57abadfd0ece1ba955fb2f684
rickards <henrique.aracomp@gmail.com>
2016-08-15 13:03:44 -0300

    Implementing early

--- development/GWVD/src/main/Generator.java
+++ development/GWVD/src/main/Generator.java
@@ -1,19 +1,51 @@
 package main;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+@SuppressWarnings("restriction")
 public class Generator extends Application{
+	
+    private double initX;
+    private double initY;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
 
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		
+		final Stage stage = new Stage(StageStyle.TRANSPARENT); 
+        Group rootGroup = new Group();
+        Scene scene = new Scene(rootGroup, 200, 200, Color.TRANSPARENT);
+        stage.setScene(scene);
+        //stage.setTitle("GENERATOR WORKLOAD VULNERABILITY DATA");
+        stage.centerOnScreen();
+        stage.show();
+        
+        //when mouse button is pressed, save the initial position of screen
+        rootGroup.setOnMousePressed((MouseEvent me) -> {
+            initX = me.getScreenX() - stage.getX();
+            initY = me.getScreenY() - stage.getY();
+        });
+
+        //when screen is dragged, translate it accordingly
+        rootGroup.setOnMouseDragged((MouseEvent me) -> {
+            stage.setX(me.getScreenX() - initX);
+            stage.setY(me.getScreenY() - initY);
+        });
+        
+        //create dragger with desired size
+        Circle dragger = new Circle(100, 100, 100);
+        
+        rootGroup.getChildren().addAll(dragger);
 	}
 
 }
