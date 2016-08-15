67196ba36b256b1b5ed04ba27b8174cc8127bb45
rickards <henrique.aracomp@gmail.com>
2016-08-15 10:53:28 -0300

    Second commit

---
+++ development/GWVD/src/main/Generator.java
@@ -0,0 +1,19 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Generator extends Application{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
